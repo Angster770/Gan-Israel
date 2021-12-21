3db3b6a31a05ccfff3a0dc7952eaf10834e1d455
Angster770 <83431968+Angster770@users.noreply.github.com>
2021-12-21 13:55:07 -0500

    Add files via upload

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,17 @@
-const ganisrael = require("../../models/ganisrael");
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
     center: ganisrael.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(ganisrael.geometry.coordinates)
+    .setPopUp(
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+            `<h3>${ganisrael.title}</h3><p>${ganisrael.location}</p>`
+        )
+    )
     .addTo(map)
--- seeds/app.js
+++ seeds/app.js
@@ -1,7 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+//
 const mongoose = require('mongoose')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
 const Ganisrael = require('../models/ganisrael')
+    ///
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 mongoose.connect('mongodb://localhost:27017/gan-israel')
     .then(() => {
@@ -19,12 +27,18 @@ const seedDB = async() => {
     for (let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+        const location = `${cities[rand1000].city}, ${cities[rand1000].state}`
+        const geoData = await geocoder.forwardGeocode({
+            query: location,
+            limit: 1
+        }).send()
         const gan = new Ganisrael({
             author: '61ba2ed074e6f50f9ff951a0',
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, animi architecto mollitia repellat ex quia sequi asperiores soluta dolorem suscipit, maiores laudantium, rem officia? Dignissimos ut harum accusamus ad velit.',
             price,
+            geometry: geoData.body.features[0].geometry,
             images: [{
                     url: 'https://res.cloudinary.com/dzmgiq9rb/image/upload/v1639954311/GanIsrael/fhajcdxygsuqvqhztme8.png',
                     filename: 'GanIsrael/fhajcdxygsuqvqhztme8'
